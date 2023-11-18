93aabaaf07471bb90cf24456f035a88dce875e84
Tyuiu.IvanovJD@outlook.com <yarikiro@mail.ru>
2023-11-18 15:57:58 +0300

    Task3

--- Tyuiu.IvanovJD.Sprint5.Task2.V25.Test/DataServiceTest.cs
+++ Tyuiu.IvanovJD.Sprint5.Task2.V25.Test/DataServiceTest.cs
@@ -9,11 +9,11 @@ namespace Tyuiu.IvanovJD.Sprint5.Task2.V25.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void CheckExistsFile()
+        public void TestMethod1()
         {
             string path = $@"C:\Users\yarik\source\repos\Tyuiu.IvanovJD.Sprint5\Tyuiu.IvanovJD.Sprint5.Task2.V25\bin\Debug\OutPutFileTask2.txt";
-            FileInfo fileinfo = new FileInfo(path);
-            bool fileExists = fileinfo.Exists;
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
             bool wait = true;
             Assert.AreEqual(wait, fileExists);
         }
---
+++ Tyuiu.IvanovJD.Sprint5.Task3.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.IvanovJD.Sprint5.Task3.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.IvanovJD.Sprint5.Task3.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d180078c-f156-4968-8054-36f493b9dd0c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
